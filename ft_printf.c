2f8fad1b44598bd828ef250670350171a68233cc
root <root@localhost.localdomain>
2022-12-13 16:23:09 +0000

    initial commit

--- ft_printf.c
+++ ft_printf.c
@@ -1,9 +1,40 @@
-#include "printf.h"
+# include "printf.h"
+# include <stdarg.h>
+# include <unistd.h>
 
-static int  ft_get_format(char param, va_list args)
+
+int	ft_print_c(int c)
 {
-    if (param == 'c')
-        return (ft_printf_c(va_arg(args, int)));
+	write(1, &c, 1);
+	return (1);
+}
+
+char ft_print_str(char *str)
+{
+    size_t  i;
+
+    if (str == 0)
+    {
+        write(1, "(null)", 6);
+        return (6);
+    }
+    while(str[i])
+    {
+        write(1, &str[i], 1);
+        i++;
+    }
+    return (i);
+}
+
+static int  ft_get_format(char format, va_list args)
+{
+    if (format == 'c')
+        return (ft_print_c(va_arg(args, int)));
+    else if (format == 's')
+        return (ft_print_str(va_arg(args, char *)));
+    else if (format == '%')
+        write(1, "%", 1);
+
     else
         return (0);
 }
@@ -35,5 +66,8 @@ int ft_printf(const char *str, ...)
 
 int main()
 {
-    ft_printf("hey");
+    int c = 'x';
+    char *str = " hallo bist du dumm ";
+    ft_printf("%s ey %c", str, c);
+    write(1, "\n", 2);
 }
